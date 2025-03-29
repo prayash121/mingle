104f5a7dbe506f8039486e5db79b1cb6cb959262
prayash121 <prakharyashaswi35@gmail.com>
2025-03-29 10:23:44 +0530

    Initial commit

--- app.py
+++ app.py
@@ -1,34 +1,17 @@
 from flask import Flask, render_template
-from flask_socketio import SocketIO, send, emit
-import random
-import datetime
+from flask_socketio import SocketIO
 
 app = Flask(__name__)
-socketio = SocketIO(app)
-
-users = []
+socketio = SocketIO(app, cors_allowed_origins="*")  # Allow connections from any device
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
-@socketio.on("connect")
-def handle_connect():
-    users.append(request.sid)
-    if len(users) >= 2:  # If at least 2 users are online
-        user1, user2 = users[:2]
-        emit("message", {"message": "You have been connected to a stranger!", "timestamp": get_time()}, room=user1)
-        emit("message", {"message": "You have been connected to a stranger!", "timestamp": get_time()}, room=user2)
-        users.remove(user1)
-        users.remove(user2)
-
 @socketio.on("message")
 def handle_message(msg):
-    timestamp = get_time()
-    emit("message", {"message": msg, "timestamp": timestamp}, broadcast=True)  # Sends to all users
-
-def get_time():
-    return datetime.datetime.now().strftime("%H:%M:%S")  # Format HH:MM:SS
+    print(f"Message received: {msg}")  # Debugging log
+    socketio.send(msg, broadcast=True)  # Broadcast message to all connected users
 
 if __name__ == "__main__":
-    socketio.run(app, debug=True)
+    socketio.run(app, host="0.0.0.0", port=5000, debug=True)
